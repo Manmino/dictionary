159100a10309088ffaadb75ebc5a816a59300501
trosel <1299322+trosel@users.noreply.github.com>
2024-07-31 20:20:29 -0700

    New dictionary format + multi-langauge

--- js/dict.js
+++ js/dict.js
@@ -1,5 +1,13 @@
-var dictionary, re;
-var tables = ["#results_manmino", "#results_english"];
+let dictionary, language, re, qrcode;
+const tables = ["#results_manmino", "#results_nat"];
+const languages = {
+	en:	["English",	"English",	1],
+	kr:	["Korean",	"Korean",	4],
+	jp:	["Japanese",	"Japanese",	5],
+	zh: ["Chinese",	"Chinese",	7],
+	tg:	["Tagalog",	"Tagalog",	11],
+	sa:	["Sanskrit",	"Sanskrit",	6],
+};
 
 $(document).ready(function() {
 	$("#searchfield").focus();
@@ -14,61 +22,128 @@ $(document).ready(function() {
 		$("#searchfield").focus();
 	});
 
-	Papa.parse('data/ManminoDict.tsv', {
-		download: true,
-		header: false,
-		delimiter: '\t',
-		skipEmptyLines: true,
-		complete: function(results) {
-			dictionary = results.data;
+	$("#dictselector").on("change", function() {
+		loadDict($( this ).val(), true);
+	});
+
+	$("input:checkbox").on("change", function() {
+		doSearch(true);
+	});
+
+	$(window).on("popstate", function(e) {
+		const state = e.originalEvent.state;
+		if(state) {
+			$("#searchfield").val(state.q);
+
+			if(state.d != language)
+				loadDict(state.d, false);
+			else
+				doSearch(false);
 		}
 	});
+
+	const sp = new URLSearchParams(location.search);
+	if(sp.has("q"))
+		$("#searchfield").val(sp.get("q"));
+
+	if(sp.has("d") && sp.get("d") in languages)
+		language = sp.get("d");
+	else
+		language = localStorage.getItem("language");
+
+	if(!language)
+		language = "en";
+
+	history.replaceState({ d: language, q: $("#searchfield").val() }, "", document.location.href);
+	loadDict(language, false);
 });
 
-function searchEnglish(entry) {
-	// entry[number] ... number is the column number of the table
-	return re.test(entry[1].toLowerCase()); 
+function loadDict(lang, history) {
+	language = lang;
+	Papa.parse("data/manmino.csv", {
+		download: true,
+		header: false,
+		skipEmptyLines: true,
+		complete: function(results) {
+			dictionary = results.data.map((entry) => [entry[0] + " (" + entry[2] + ") ", entry[languages[language][2]]]);
+
+			if(history)
+				pushHistory();
+
+			$(".natlang").text(languages[language][0]);
+			$(".natlang_lower").text(languages[language][1]);
+			$("#dictselector").val(language);
+
+			localStorage.setItem("language", language);
+
+			doSearch(false);
+		}
+	});
 }
 
-function searchManmino(entry) {
-	return re.test(entry[0].toLowerCase());
+function pushHistory() {
+	const url = new URL(location);
+	url.searchParams.set("d", language);
+	if($("#searchfield").val().length > 0)
+		url.searchParams.set("q", $("#searchfield").val());
+	else
+		if(url.searchParams.has("q"))
+			url.searchParams.delete("q");
+	history.pushState({ d: language, q: $("#searchfield").val() }, "", url);
 }
 
-function doSearch() {
+function searchGer(entry) {
+	return re.test(entry[0]);
+}
+
+function searchNat(entry) {
+	return re.test(entry[1]);
+}
+
+function doSearch(history) {
 	$("#searchfield").focus();
 	$("#searchfield").select();
+	$(".results_table").hide();
+	$("#noresults").hide();
 
-	var query = $("#searchfield").val().trim().toLowerCase().replace(/[.*+?^${}()|[\]\\]/g, "\\$&").replace(/\\\*/g, ".*?");
+	if(qrcode)
+		hideQR();
+
+	if(history)
+		pushHistory();
+
+	const query_raw = $("#searchfield").val();
+	const query = query_raw.trim().replace(/[.+?^${}()|[\]\\]/g, "").replace(/\*/g, "?").split("").join("'*").replace(/\?/g, "[\\w']*");
 
 	if(query.length > 0) {
-		$(".results_table").hide();
-		re = new RegExp("(?<!\\p{L})" + query + "(?!\\p{L})", "u");
+		re = new RegExp("(^|\\P{L})(" + query + ")($|\\P{L})", "ui");
 
-		var results = [[], []]
-		if($("#search_english").is(":checked"))
-			results[1] = dictionary.filter(searchEnglish);
+		const results = [[], []]
 		if($("#search_manmino").is(":checked"))
-			results[0] = dictionary.filter(searchManmino);
+			results[0] = dictionary.filter(searchGer);
+		if($("#search_nat").is(":checked"))
+			results[1] = dictionary.filter(searchNat);
+
+		$(document).prop("title", query_raw + " – Manmino Dictionary");
 
 		if(results[0].length > 0 || results[1].length > 0) {
-			$("#noresults").hide();
 			$(".results_table tr:has(td)").remove();
-			$(document).prop("title", $("#searchfield").val() + " - ManminoDict");
 
-			var re_start = new RegExp("^\\b" + query + "\\b");
+			const re_start = new RegExp("^" + query + "($|\\P{L})", "ui");
 			results.forEach(function(ra, i) {
 				if(ra.length > 0) {
-					var entries = [[], []];
-					ra.sort(function(a, b){return a[i] < b[i] ? -1 : 1});
+					const entries = [[], []];
+					const highlight = $("#highlight_term").is(":checked");
+					ra.sort(function(a, b){return a[i].toLowerCase() < b[i].toLowerCase() ? -1 : 1});
 					ra.forEach(function(r) {
-						if(re_start.test(r[i].toLowerCase()))
+						if(re_start.test(r[i]))
 							entries[0].push(r);
 						else
 							entries[1].push(r);
 					});
 					entries.forEach(function(e) {
 						e.forEach(function(r) {
-							$(tables[i]).append("<tr><td>" + r[i] + "</td><td>" + r[1-i] + "</td><td>" + r[2] + "</td><td>" + r[3] + "</td></tr>");
+							$(tables[i]).append("<tr><td>" + (highlight ? r[i].replace(re, "$1<b>$2</b>$3") : r[i]) + "</td><td>" + r[1-i] + "</td></tr>");
 						});
 					});
 					$(tables[i]).show();
@@ -77,7 +152,32 @@ function doSearch() {
 		}
 		else {
 			$("#noresults").show();
-			$(document).prop("title", "ManminoDict");
 		}
 	}
+	else
+		$(document).prop("title", "Manmino Dictionary");
+}
+
+function showQR() {
+	if(!qrcode) {
+		qrcode = new QRCode(document.getElementById("qrcode"), {
+			text: document.location.href,
+			width: 150,
+			height: 150,
+			colorDark: "#3d6a4a",
+			colorLight: "#ffffff"
+		});
+	}
+	else {
+		qrcode.makeCode(document.location.href);
+	}
+
+	$("#logo").hide();
+	$("#qrcode").css("display", "inline-block");
+}
+
+function hideQR() {
+	$("#qrcode").hide();
+	$("#logo").show();
+	qrcode.clear();
 }
